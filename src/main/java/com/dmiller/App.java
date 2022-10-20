371d4787b596181c22e0e6784d1a9e133975ca41
dmiller <dustin.e.miller@gmail.com>
2022-10-19 20:53:22 -0400

    first commit

---
+++ src/main/java/com/dmiller/App.java
@@ -0,0 +1,13 @@
+package com.dmiller;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
